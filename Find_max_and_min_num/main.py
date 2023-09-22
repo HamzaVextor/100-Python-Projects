adba97fa26f87d3777cb02e8d6b2475542d4b587
Hamza_Vextor <122175521+HamzaVextor@users.noreply.github.com>
2023-09-22 17:54:38 +0500

    Create main.py

---
+++ Find_max_and_min_num/main.py
@@ -0,0 +1,15 @@
+# Random Values
+var1 = [37, 92, 14, 63, 51, 27, 84, 6, 70, 45, 19, 55, 8, 98, 31, 73, 42, 67, 12, 59, 23, 78, 
+        3, 87, 60, 16, 89, 5, 41, 64, 26, 76, 30, 48, 21, 68, 10, 57, 2, 93, 
+        54, 39, 80, 35, 74, 1, 62, 32, 91, 18, 58, 22, 69, 9, 56, 36, 79, 13, 
+        72, 4, 85, 50, 24, 81, 28, 65, 11, 66, 33, 77, 7, 86, 20, 53, 15, 71, 40, 75, 25, 
+        88, 38, 34, 61, 17, 82, 43, 97, 47, 94, 29, 46, 99, 44, 95, 49, 96, 90, 52, 100, 31]
+
+# Find the maximum value
+var2 = max(var1)
+
+# Find the manimum value
+var3 = min(var1)
+
+# Print the minimum and maximum values
+print(f"The largest value is {var2} and the smallest value is {var3}")
