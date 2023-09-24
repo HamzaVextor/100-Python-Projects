adc3b04785375a6ce1ab631c2309e22a95a30618
Hamza_Vextor <122175521+HamzaVextor@users.noreply.github.com>
2023-09-24 16:07:23 +0500

    Update main.py

--- Quiz App/main.py
+++ Quiz App/main.py
@@ -1,127 +1,111 @@
 import sys
 
+def print_question(questions, options):
+    print(questions)
+    for option in options:
+        print(option)
+    return input('Please select an option: ')
+
+def display_score(score):
+    print(f'The score is {score}')
+
 def easy_game():
     score = 0
 
-    # Question 1
-    print("Q1. What does HTML stand for")
-    print('''
-        a) Hyperlink Text Markup Language
-        b) Hyper Text Makeup Language
-        c) Hyper Text Markup Language
-        d) High Tech Modern Language
-    ''')
-    
-    q1_input = input("Please select an option: ")
-    if q1_input == 'b':
-        score += 100
-        print(f'You got {score} points')
-    elif q1_input != 'b':
-        score -= 100
-        print(score)
-    else:
-        print('wrong choice')
+    questions = [
+        {
+            'question': 'Q1. What does HTML stand for?',
+            'options': [
+                "a) Hyperlink Text Markup Language",
+                "b) Hyper Text Makeup Language",
+                "c) Hyper Text Markup Language",
+                "d) High Tech Modern Language",
+            ],
+            'correct_answer': 'b'
+        },
+        {
+            "question": "Q2. Which of the following is a front-end programming language?",
+            "options": [
+                "a) Java",
+                "b) Python",
+                "c) HTML",
+                "d) PHP",
+            ],
+            "correct_answer": "c",
+        },
+        {
+            "question": "Q3. What does API stand for?",
+            "options": [
+                "a) Application Programming Interface",
+                "b) Advanced Programming Interface",
+                "c) Automated Process Integration",
+                "d) Application Process Interface",
+            ],
+            "correct_answer": "a",
+        },
+    ]
 
-    # Question 2
-    print("Q2. Which of the following is a front-end programming language?")
-    print('''
-        a) Java
-        b) Python
-        c) HTML
-        d) PHP
-    ''')
-    
-    q2_input = input("Please select an option: ")
-    if q2_input == 'c':
-        score += 100
-        print(f'You got {score} points')
-    elif q2_input != 'c':
-        score -= 100
-        print(score)
-    else:
-        print('wrong choice')
+    for question_data in questions:
+        question = question_data['question']
+        option = question_data['options']
+        correct_answer = question_data['correct_answer']
 
-    # Question 3
-    print('Q3. What does API stand for?')
-    print('''
-        a) Application Programming Interface
-        b) Advanced Programming Interface
-        c) Automated Process Integration
-        d) Application Process Interface
-    ''')
-    
-    q3_input = input("Please select an option: ")
-    if q3_input == 'a':
-        score += 100
-        print(f'You got {score} points')
-    elif q3_input != 'a':
-        score -= 100
-        print('You got', score)
-    else:
-        print('wrong choice')
-
-
+        user_input = print_question(question, option)
+        if user_input == correct_answer:
+            score += 100
+            display_score(score)
+        else:
+            score -= 100
+            display_score(score)
 
 def hard_game():
     score = 0
 
-    # Question 1
-    print("Q1. What is the time complexity of a binary search algorithm?")
-    print('''
-    a) O(1)
-    b) O(log n)
-    c) O(n)
-    d) O(n^2)
-    ''')
-    
-    q1_input = input("Please select an option: ")
-    if q1_input == 'b':
-        score += 100
-        print(f'You got {score} points')
-    elif q1_input != 'b':
-        score -= 100
-        print(score)
-    else:
-        print('wrong choice')
+    questions = [
+        {
+            'question': 'Q1. What is the time complexity of a binary search algorithm?',
+            'options': [
+                "a) O(1)",
+                "b) O(log n)",
+                "c) O(n)",
+                "d) O(n^2)",
+            ],
+            'correct_answer': 'b'
+        },
+        {
+            "question": "Q2. Which of the following design patterns is commonly used for database connection management?",
+            "options": [
+                "a) Singleton",
+                "b) Factory",
+                "c) Observer",
+                "d) Adapter",
+            ],
+            "correct_answer": "a",
+        },
+        {
+            "question": "Q3. Which HTTP status code represents a successful response in a REST API?",
+            "options": [
+                "a) 200 OK",
+                "b) 400 Bad Request",
+                "c) 404 Not Found",
+                "d) 500 Internal Server Error",
+            ],
+            "correct_answer": "a",
+        },
+    ]
 
-    # Question 2
-    print("Q2. Which of the following design patterns is commonly used for database connection management?")
-    print('''
-        a) Singleton
-        b) Factory
-        c) Observer
-        d) Adapter
-    ''')
-    
-    q2_input = input("Please select an option: ")
-    if q2_input == 'a':
-        score += 100
-        print(f'You got {score} points')
-    elif q2_input != 'a':
-        score -= 100
-        print(score)
-    else:
-        print('wrong choice')
+    for question_data in questions:
+        question = question_data['question']
+        option = question_data['options']
+        correct_answer = question_data['correct_answer']
 
-    # Question 3
-    print('Q3. Which HTTP status code represents a successful response in a REST API?')
-    print('''
-a) 200 OK
-b) 400 Bad Request
-c) 404 Not Found
-d) 500 Internal Server Error
-    ''')
-    
-    q3_input = input("Please select an option: ")
-    if q3_input == 'a':
-        score += 100
-        print(f'You got {score} points')
-    elif q3_input != 'a':
-        score -= 100
-        print(score)
-    else:
-        print('wrong choice')
-
+        user_input = print_question(question, option)
+        if user_input == correct_answer:
+            score += 100
+            display_score(score)
+        else:
+            score -= 100
+            display_score(score)
 
 def main():
     print('Welcome to dev quiz game.')
