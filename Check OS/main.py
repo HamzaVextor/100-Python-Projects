e8db2900826a5764e55693a965598f071520c8f0
Hamza_Vextor <122175521+HamzaVextor@users.noreply.github.com>
2023-09-30 23:11:18 +0500

    Update main.py

--- Check OS/main.py
+++ Check OS/main.py
@@ -1,3 +1,13 @@
 import platform
-check_details = platform.system()
-print(check_details)
+
+def print_system_info():
+    # Print system information
+    print(f"System: {platform.system()}")
+    print(f"Version: {platform.version()}")
+    print(f"Release: {platform.release()}")
+    print(f"Platform: {platform.platform()}")
+    print(f"Processor: {platform.processor()}")
+    print(f"Windows Version: {platform.win32_ver()}")
+
+# Call the function to print system information
+print_system_info()
