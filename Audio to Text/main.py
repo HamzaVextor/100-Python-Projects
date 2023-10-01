32a92b950aaff850f6e68f1242e08c2a7906f9bb
Hamza_Vextor <122175521+HamzaVextor@users.noreply.github.com>
2023-10-01 16:58:34 +0500

    Update main.py

--- Audio to Text/main.py
+++ Audio to Text/main.py
@@ -1,14 +1,35 @@
+Certainly! Here's your code with added comments to document its functionality:
+
+```python
 import speech_recognition as sr
 
 def main():
+    # Create a recognizer object
     r = sr.Recognizer()
+
+    # Open the microphone as a source for audio input
     with sr.Microphone() as source:
+        # Set a pause threshold for recognizing speech (1 second)
         r.pause_threshold = 1
+
+        # Record audio from the microphone
         audio = r.listen(source)
+
+        # Use Google's speech recognition API to convert audio to text (language set to 'en-pk')
         query = r.recognize_google(audio, language='en-pk')
+
+        # Print the recognized text to the console
         print(f'User said: {query}')
+
+    # Open a file named 'text.txt' and write the recognized text to it
     with open('text.txt', 'w') as f:
         f.write(query)
+
+    # Return the recognized text
     return query
 
+# Call the main function to start speech recognition
 main()
+```
+
+These comments explain the purpose and functionality of each section of your code. It's essential to document your code like this to make it more understandable for yourself and others who may read or work on it in the future.
