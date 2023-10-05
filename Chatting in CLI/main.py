a26e8788ff2476727dc56020d017f93f9e145d50
HamzaAsghar <122175521+HamzaVextor@users.noreply.github.com>
2023-10-05 18:42:43 +0500

    Add files via upload

---
+++ Chatting in CLI/main.py
@@ -0,0 +1,9 @@
+user_name_1 = input('Enter your name: ')
+user_name_2 = input('Enter your name: ')
+
+while True: 
+ 
+    chat1 = input(f'{user_name_1}: Enter a message: ')
+    print(chat1)
+    chat2 = input(f'{user_name_2}: Enter a message: ')
+    print(chat2)
